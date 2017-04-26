1d5c2188fb41885220c66f4241b56e98de0099af
ducng <duc.nguyen@spc-vn.com>
2017-04-26 19:12:22 +0700

    add function check last login

--- Controller/ClientsController.php
+++ Controller/ClientsController.php
@@ -98,7 +98,7 @@ class ClientsController extends AppController {
 	        
 	        $this->TblMstepClientRequest->unbindFully();
 	        $request = $this->TblMstepClientRequest->findById($request_id);
-	        if(!is_numeric($request_id) || !$request || $request['TblMstepClientRequest']['status'] != 'requested' || $request['TblMstepClientRequest']['client_id'] > 0){
+	        if(!is_numeric($request_id) || !$request || $request['TblMstepClientRequest']['client_id'] > 0){
 	            throw new NotFoundException();
 	        }else{
 	            $is_request = true;
